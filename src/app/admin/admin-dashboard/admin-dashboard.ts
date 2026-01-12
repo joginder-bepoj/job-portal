125f494b3006ef64fa19d8aabdba951e9a8ebf27
Joginder <joginder.director@gmail.com>
2026-01-12 18:47:42 +0530

    admin changes

--- src/app/admin/admin-dashboard/admin-dashboard.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.scss',
 })
